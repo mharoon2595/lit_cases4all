dffd4ee691789a46446c68a01eec74d97f1c661f
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-10-25 22:58:08 +0530

    config step 2 almost done

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,9 +1,27 @@
+import { notFound } from "next/navigation";
 import { PageProps } from "../../../../.next/types/app/layout";
+import { db } from "@/db";
+import DesignConfigurator from "./DesignConfigurator";
 
 const page = async ({ searchParams }: PageProps) => {
   const { id } = searchParams;
+  if (!id || typeof id !== "string") return notFound();
 
-  return <div>{id}</div>;
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) return notFound();
+
+  const { url, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imgDim={{ width, height }}
+      url={url}
+    />
+  );
 };
 
 export default page;
