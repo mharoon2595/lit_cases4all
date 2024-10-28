664467953b36b0ac91da4d23723fd2b88b2a209e
Haroon <131857305+mharoon2595@users.noreply.github.com>
2024-10-28 21:38:54 +0530

    minor tweak

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,9 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <section className="my-auto w-full flex justify-center items-center">
-      <SignIn fallbackRedirectUrl={"http://localhost:3000/auth-callback"} />
+      <SignIn
+        fallbackRedirectUrl={`${process.env.NEXT_PUBLIC_SERVER_URL}/auth-callback`}
+      />
     </section>
   );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,9 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <section className="my-5 w-full flex justify-center items-center">
-      <SignUp fallbackRedirectUrl={"http://localhost:3000/auth-callback"} />
+      <SignUp
+        fallbackRedirectUrl={`${process.env.NEXT_PUBLIC_SERVER_URL}/auth-callback`}
+      />
     </section>
   );
 }
